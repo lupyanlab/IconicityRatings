21e589b3894882f3458a382b9d1d36a40075f274
Kevin Mui <kmui@sapir.psych.wisc.edu>
2019-08-01 18:53:09 -0500

    add module support to prot.js in prod

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,34 @@
-const gulp = require('gulp');
-const htmlreplace = require('gulp-html-replace');
+const gulp = require("gulp");
+const htmlreplace = require("gulp-html-replace");
 
-gulp.task('default', function() {
-    console.log("hello world");
-})
+gulp.task("default", function() {
+  console.log("hello world");
+});
 
-gulp.task('copy', function() {
-    gulp.src(['dev/**/*','!dev/index.html','!dev/dev.js', '!dev/images'])
-        .pipe(gulp.dest('prod'));
-})
+gulp.task("copy", function() {
+  return gulp
+    .src(["dev/**/*", "!dev/index.html", "!dev/dev.js", "!dev/images"])
+    .pipe(gulp.dest("prod"));
+});
 
-gulp.task('switchjs', function() {
-    gulp.src('dev/index.html')
-        .pipe(htmlreplace({
-            js: {
-                src: [['module', 'prod.js']],
-                tpl: '<script type="%s" src="%s"></script>',
-            },
-        }))
-        .pipe(gulp.dest('prod'));
-})
+gulp.task("switchjs", function() {
+  return gulp
+    .src("dev/index.html")
+    .pipe(
+      htmlreplace({
+        js: {
+          src: "prod.js",
+          tpl: '<script type="module" src="%s"></script>',
+        },
+        form: ""
+      })
+    )
+    .pipe(gulp.dest("prod"));
+});
 
-gulp.task('prod', ['copy', 'switchjs']);
+gulp.task("prod", gulp.series(["copy", "switchjs"]));
 
-gulp.task('watch', ['copy', 'switchjs'], function() {
-    gulp.watch(['dev/**/*','!dev/index.html','!dev/dev.js'], ['copy']);
-    gulp.watch('dev/index.html', ['switchjs']);
-})
+gulp.task("watch", gulp.series(["copy", "switchjs"]), function() {
+  gulp.watch(["dev/**/*", "!dev/index.html", "!dev/dev.js"], ["copy"]);
+  gulp.watch("dev/index.html", ["switchjs"]);
+});
